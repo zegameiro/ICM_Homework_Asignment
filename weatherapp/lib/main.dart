47070bd156044a897fec41658f4172599df44e03
zegameiro <jose.mcgameiro@ua.pt>
2024-03-03 00:26:17 +0000

    Finished HomePage design

--- weatherapp/lib/main.dart
+++ weatherapp/lib/main.dart
@@ -17,7 +17,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData.dark(
         useMaterial3: true,
-        
       ),
       home: const NavigationBarExample(),
     );
